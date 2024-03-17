add10d6aa4a1e1e8f3324512e05ccd12509ea3a6
MiriMichaelyy <163027427+MiriMichaelyy@users.noreply.github.com>
2024-03-17 16:47:23 +0200

    Update outputs.py
    updated plot_outputs fn:
    made similar to article logic
    changed some variable names

--- Article/outputs.py
+++ Article/outputs.py
@@ -61,23 +61,34 @@ def smooth_curve_dis(points, factor=0.3):
             smoothed_points.append(point)
     return smoothed_points
 
-def plot_outputs(Disc_loss_real=None, Disc_loss_fake=None, Gen_loss=None):
-    if Disc_loss_real is None:
-        Disc_loss_real = numpy.load('Disc_loss_real.npy')
-    if Disc_loss_fake is None:
-        Disc_loss_fake = numpy.load('Disc_loss_fake.npy')
-    if Gen_loss is None:
-        Gen_loss = numpy.load('Gen_loss.npy')
+def plot_outputs(Disc_real=None, Disc_fake=None, Gen=None):
+    if Disc_real is None:
+        Disc_real = numpy.load('Disc_loss_real.npy')
+    if Disc_fake is None:
+        Disc_fake = numpy.load('Disc_loss_fake.npy')
+    if Gen is None:
+        Gen = numpy.load('Gen_loss.npy')
+
+    Disc_real_loss = []
+    Disc_fake_loss = []
+    Gen_loss = []
+
+    for i in range(len(Gen)):
+        if i % 450 == 0:
+            Gen_loss.append(Gen[i])
+            Disc_fake_loss.append(Disc_fake[i])
+            Disc_real_loss.append(Disc_real[i])
+    
 
     epoch = range(0, len(Disc_loss_real))
     fig, ax = plt.subplots(figsize=(6, 4))
-    plt.plot(epoch, Disc_loss_real, linewidth=0.8, color='#9b0000', marker='.')
+    plt.plot(epoch, Disc_real_loss, linewidth=0.8, color='#9b0000', marker='.')
     plt.title('Discriminator loss real')
     plt.xlabel('epoch')
     plt.ylabel('Loss')
     plt.show()
     fig, ax = plt.subplots(figsize=(6, 4))
-    plt.plot(epoch, smooth_curve_dis(Disc_loss_fake), linewidth=0.8, color='#1562ff', marker='.')
+    plt.plot(epoch, smooth_curve_dis(Disc_fake_loss), linewidth=0.8, color='#1562ff', marker='.')
     plt.title('Discriminator loss fake')
     plt.xlabel('epoch')
     plt.ylabel('Loss')
